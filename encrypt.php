39a8f2fec4222205989592b79927a764dd394545
Saikat Chakrabortty <saikatchakrabortty2@gmail.com>
2016-08-21 16:22:40 +0530

    updated encryption

--- encrypt.php
+++ encrypt.php
@@ -1,8 +1,10 @@
 <?php
 
-$string = "i'm encrypted'";
-$secret_key = "fasgdftrtyhgbhnj";
+$string = "i'm encrypted'"; //the string to be encryptedd
 
+$secret_key = "fasgdftrtyhgbhnj"; //the secret key 
+
+//dont change  this function 
 $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND);
 
 $encrypted_string = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $secret_key, $string, MCRYPT_MODE_CBC, $iv);
